84070805351cdc6ab9e80df29926dffa773d7e07
pa-tiq <patrickdsilva99@gmail.com>
2023-05-05 11:23:06 -0400

    user model

---
+++ backend/src/main/java/com/dantas/backend/models/User.java
@@ -0,0 +1,71 @@
+package com.dantas.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 180, nullable = false)
+    private String email;
+
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @Column(length = 65535)
+    private String comment;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+}
